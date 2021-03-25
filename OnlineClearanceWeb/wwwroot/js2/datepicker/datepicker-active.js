b1333f61b487ea7883fd64af4bcc12370c6a3bf8
kenny20200127 <ilekhaize_kenneth@yahoo.com>
2021-03-25 11:22:23 +0100

    comiit

--- OnlineClearanceWeb/wwwroot/js2/datepicker/datepicker-active.js
+++
@@ -1,3 +0,0 @@
-(function($){"use strict";$('#start').datepicker({dateFormat:'dd.mm.yy',prevText:'<i class="fa fa-chevron-left"></i>',nextText:'<i class="fa fa-chevron-right"></i>',onSelect:function(selectedDate)
-{$('#finish').datepicker('option','minDate',selectedDate);}});$('#finish').datepicker({dateFormat:'dd.mm.yy',prevText:'<i class="fa fa-chevron-left"></i>',nextText:'<i class="fa fa-chevron-right"></i>',onSelect:function(selectedDate)
-{$('#start').datepicker('option','maxDate',selectedDate);}});})(jQuery);
--- OnlineClearanceWeb/wwwroot/js2/metisMenu/metisMenu-active.js
+++
@@ -1,1 +0,0 @@
-(function($){"use strict";$('#menu1').metisMenu();})(jQuery);
